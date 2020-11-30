d370eb81d3c8416f9d2148bab1449faada43b1b9
MsDiala <diala-is-diala@hotmail.com>
2020-11-30 16:36:21 +0200

    aa

--- math_series/math_series.py
+++ math_series/math_series.py
@@ -1,24 +1,26 @@
-def fibonacci(n):
+def fibonacci(n): ##Function that returns value of the given index from Fibonacci sequence 
     '''
     Given an integer value n, this function returns the nth value of the Fibonacci Numbers.
     '''
     if isinstance(n, str):
-        raise TypeError("n must be an integer greater than or equal to 0.")
+        #The isinstance() function returns True if the specified object 
+        #is of the specified type, otherwise False.
+        raise TypeError("n must be an integer greater than or equal to 0.") # The raise keyword is used to raise an exception.
     elif n < 0:
         prompt = "n must be an integer greater than or equal to 0."
         return prompt
     elif n == 0:
-        value = 0
-        return value
+         fibonacciOfN = 0
+         return  fibonacciOfN 
     elif n == 1 or n == 2:
-        value = 1
-        return value
+         fibonacciOfN  = 1
+         return  fibonacciOfN 
     elif n > 2:
-        value = fibonacci(n-1) + fibonacci(n-2)
-        return value
+        fibonacciOfN = fibonacci(n-1) + fibonacci(n-2)
+        return  fibonacciOfN 
 
 
-def lucas(n):
+def lucas(n): ###Function that returns value of the given index from Lucas sequence
     '''
     Given an integer value n, this function returns the nth value of the Lucas Numbers.
     '''
@@ -26,30 +28,30 @@ def lucas(n):
         prompt = "n must be an integer greater than or equal to 0."
         return prompt
     elif n == 0:
-        value = 2
-        return value
+        lucasOfN = 2
+        return lucasOfN
     elif n == 1:
-        value = 1
-        return value
+        lucasOfN = 1
+        return lucasOfN
     elif n >= 2:
-        value = lucas(n-1) + lucas(n-2)
-        return value
+        lucasOfN = lucas(n-1) + lucas(n-2)
+        return lucasOfN
 
 
-def sum_series(n, id1=0, id2=1):
+def sum_series(n, arg1=0, arg2=1): ###Function that returns value of the given index from Custom fibonacci-like sequence
     # pass
     '''
-    Given an integer value n and two optional terms id1 and id2, this function returns the nth value of the user-specified Fibonacci or Lucas numbers (defaults to Fibonacci if no optional terms provided).
+    Given an integer value n and two optional terms arg1 and arg2, this function returns the nth value of the user-specified Fibonacci or Lucas numbers (defaults to Fibonacci if no optional terms provarged).
     '''
     if n < 0:
         prompt = "n must be an integer greater than or equal to 0."
         return prompt
-    elif id1 == 0 and id2 == 1:
-        value = fibonacci(n)
-        return value
-    elif id1 == 2 and id2 == 1:
-        value = lucas(n)
-        return value
+    elif arg1 == 0 and arg2 == 1:
+        fibonacciOfN = fibonacci(n)
+        return fibonacciOfN
+    elif arg1 == 2 and arg2 == 1:
+        lucasOfN = lucas(n)
+        return lucasOfN
     else:
-        warning = "Sorry, that combination has not yet been defined."
-        return warning
+        noSum = "Sorry, that sum is not defined."
+        return noSum
