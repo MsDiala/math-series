58e4f67bbcf96c9788c576683f226047502087a6
MsDiala <diala-is-diala@hotmail.com>
2020-11-30 15:14:48 +0200

    dd

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,135 +1,114 @@
-from math_series import __version__
+import pytest
+from math_series.math_series import (
+    fibonacci,
+    lucas,
+    sum_series,
+)
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_fib_exists():
+    assert fibonacci
 
 
-    from series import fibonacci, lucas, sum_series
+def test_lucas_exists():
+    assert lucas
 
-# Fibonacci tests"
-# Expected Outcome
 
-def test_zero():
+def test_sum_series_exists():
+    assert sum_series
+
+
+# *************  Fibonacci series tests  ************
+
+def test_fib_0():
     expected = 0
     actual = fibonacci(0)
     assert actual == expected
 
-def test_one():
+
+def test_fib_false_pos():
+    expected = 0
+    actual = fibonacci(1)
+    assert actual != expected
+
+
+def test_fib_less_than_0():
+    expected = "n must be an integer greater than or equal to 0."
+    actual = fibonacci(-1)
+    assert actual == expected
+
+
+# @pytest.mark.skip
+def test_fib_str_0():
+    with pytest.raises(TypeError):
+        assert fibonacci("0") is TypeError
+
+
+def test_fib_int1():
     expected = 1
     actual = fibonacci(1)
     assert actual == expected
 
-def test_15n():
-    expected = 610
-    actual = fibonacci(15)
+
+def test_fib_int2():
+    expected = 1
+    actual = fibonacci(2)
     assert actual == expected
 
 
-# Edge Case
-
-def test_negative():
-    expected = 0
-    actual = fibonacci(-4)
-    assert actual == expected
-
-# Expected Failure
-
-def test_letter():
-    expected = "Input should be a one integer"
-    actual = fibonacci("a")
+def test_fib_int5():
+    expected = 2 + 3
+    actual = fibonacci(5)
     assert actual == expected
 
 
-def test_float():
-    expected = "Input should be a one integer"
-    actual = fibonacci(1.354)
-    assert actual == expected
+# *************  Lucas series tests  ************
 
-
-# LUCAS TESTS
-# Expected Outcome
-def test_zero_lucas():
+def test_luc_0_pass():
     expected = 2
     actual = lucas(0)
     assert actual == expected
 
-def test_one_lucas():
-    expected = 1
+
+def test_luc_0_false_pos():
+    expected = 2
     actual = lucas(1)
+    assert actual != expected
+
+
+def test_luc_less_than_0():
+    expected = "n must be an integer greater than or equal to 0."
+    actual = lucas(-1)
     assert actual == expected
 
-def test_three_lucas():
-    expected = 4
-    actual = lucas(3)
-    assert actual == expected
 
-def test_four_lucas():
-    expected = 7
+def test_luc_4():
+    expected = 3 + 4
     actual = lucas(4)
     assert actual == expected
 
-def test_15n_lucas():
-    expected = 1364
-    actual = lucas(15)
-    assert actual == expected
 
-# Edge case
-def test_negative_lucas():
-    expected = 2
-    actual = lucas(-4)
-    assert actual == expected
+# *************  Sum series tests  ************
 
-
-# expected failure
-def test_15n_lucas():
-    expected = "Input should be a one integer"
-    actual = lucas("a")
-    assert actual == expected
-
-
-# SUM_SERIES TESTS
-# Expected Outcome
-def test_zero_sum_series_fibonacci():
-    expected = 0
-    actual = sum_series(0)
-    assert actual == expected
-
-def test_zero_sum_series_fibonacci_params():
+def test_sum_series_fib_0():
     expected = 0
     actual = sum_series(0, 0, 1)
     assert actual == expected
 
 
-def test_zero_sum_series_lucas():
+def test_sum_series_lucas_0():
     expected = 2
     actual = sum_series(0, 2, 1)
     assert actual == expected
 
-def test_sum_series_new_sequence():
-    expected = 123
-    actual = sum_series(8, 3, 4)
-    assert actual == expected
 
-# Edge Cases
-def test_sum_series_new_sequence_negative():
-    expected = 3
-    actual = sum_series(-4, 3, 4)
-    assert actual == expected
-
-def test_sum_series_new_sequence_negative_params():
-    expected = 6
-    actual = sum_series(4, -3, 4)
+def test_sum_series_less_than_zero():
+    expected = "n must be an integer greater than or equal to 0."
+    actual = sum_series(-1)
     assert actual == expected
 
 
-# expected failure
-def test_sum_series_letters():
-    expected = "Input allows only integers"
-    actual = sum_series('a', 3, 4)
-    assert actual == expected
-
-def test_sum_series_letters_in_params():
-    expected = "Input allows only integers"
-    actual = sum_series(5, 'a', 4)
+def test_sum_series_not_fib_luc():
+    expected = "Sorry, that combination has not yet been defined."
+    actual = sum_series(4, 1, 2)
     assert actual == expected
